baeca3ffa145d6c600ebe2918907c34f59946d1a
asfadani <asfa.dani0001@gmail.com>
2026-03-09 14:36:25 +0700

    Tugas modul 4.1 Basis Data

---
+++ Praktikum Basis Data/TugasModul4.1_DBMS_2505060011.sql
@@ -0,0 +1,134 @@
+-- MariaDB dump 10.19  Distrib 10.4.32-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: prak_basdat
+-- ------------------------------------------------------
+-- Server version	10.4.32-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `praktikum_1`
+--
+
+DROP TABLE IF EXISTS `praktikum_1`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `praktikum_1` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Nama` varchar(20) NOT NULL,
+  `Umur` char(2) NOT NULL,
+  `No_HP` char(15) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `praktikum_1`
+--
+
+LOCK TABLES `praktikum_1` WRITE;
+/*!40000 ALTER TABLE `praktikum_1` DISABLE KEYS */;
+INSERT INTO `praktikum_1` VALUES (1,'Asfa','19','8123456789'),(2,'Dani','19','08123456789');
+/*!40000 ALTER TABLE `praktikum_1` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `praktikum_2`
+--
+
+DROP TABLE IF EXISTS `praktikum_2`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `praktikum_2` (
+  `NPM` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Primary Key',
+  `create_time` datetime DEFAULT NULL COMMENT 'Create Time',
+  `name` varchar(50) NOT NULL,
+  `tempat_lahir` varchar(20) NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `jenis_kelamin` enum('L','P') DEFAULT NULL,
+  PRIMARY KEY (`NPM`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `praktikum_2`
+--
+
+LOCK TABLES `praktikum_2` WRITE;
+/*!40000 ALTER TABLE `praktikum_2` DISABLE KEYS */;
+/*!40000 ALTER TABLE `praktikum_2` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `praktikum_3`
+--
+
+DROP TABLE IF EXISTS `praktikum_3`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `praktikum_3` (
+  `NPM` char(5) NOT NULL,
+  `Nama` varchar(25) NOT NULL,
+  `Tempat_Lahir` varchar(30) NOT NULL,
+  `Tanggal_Lahir` date NOT NULL,
+  `Jenis_Kelamin` enum('L','P') NOT NULL,
+  `No_HP` varchar(25) NOT NULL,
+  PRIMARY KEY (`NPM`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `praktikum_3`
+--
+
+LOCK TABLES `praktikum_3` WRITE;
+/*!40000 ALTER TABLE `praktikum_3` DISABLE KEYS */;
+INSERT INTO `praktikum_3` VALUES ('21001','Budi Santoso','Jakarta','0000-00-00','L','081234567890'),('21002','Siti Aminah','Bandung','0000-00-00','P','085612345678'),('21003','Jaya Wijaya','Surabaya','0000-00-00','L','082198765432'),('21005','Doni Pratama','Semarang','0000-00-00','L','089955443322'),('21006','Ayu Lestari','Denpasar','0000-00-00','P','081344556677'),('21007','Reza Rahadian','Medan','0000-00-00','L','082233445566');
+/*!40000 ALTER TABLE `praktikum_3` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product` (
+  `product_id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(100) DEFAULT NULL,
+  `category` varchar(50) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`product_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES (1,'Laptop A','Electronics',1200.00),(2,'Smartphone B','Electronics',800.00),(3,'Tablet C','Electronics',500.00),(4,'Shirt D','Clothing',25.00),(5,'Jeans E','Clothing',45.00),(6,'Shoes F','Clothing',60.00),(7,'Book G','Books',15.00),(8,'Notebook H','Books',10.00),(9,'Pen I','Stationery',2.00),(10,'Pencil J','Stationery',1.50);
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-03-04  9:06:42
