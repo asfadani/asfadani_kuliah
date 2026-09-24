e0a055ca81f8131de8e28df59d5389df277eb089
asfadani <asfa.dani0001@gmail.com>
2026-09-25 03:41:57 +0700

    menyelesaikan tugas 6 PBO

--- Praktikum PBO/PraktikumPBO_6/src/main/java/TugasModul6/Main.java
+++ Praktikum PBO/PraktikumPBO_6/src/main/java/TugasModul6/Main.java
@@ -58,7 +58,7 @@ class Pakaian extends Produk {
     @Override
     public void hitungDiskon(double diskon) {
        long hargaPromo = (long) (this.harga - (this.harga * diskon));
-       this.harga = hargaPromo - 10000;
+       this.harga = hargaPromo - 100000;
    } 
 }
 
@@ -93,5 +93,46 @@ class Keranjang {
 }
 
 public class Main {
+    public static void main(String[] args) {
+        // Membuat objek produk produknya
+        long hargaUmum = 250000;
+        long hargaNovel = 150000;
+        long hargaHP = 20000000;
+        long hargaHoodie = 5000000;
+        
+        System.out.println("Membuat objek dari Produk produk");
+        Produk umum = new Produk("Sepatu", hargaUmum);
+        Buku novel = new Buku("Harry Potter", hargaNovel);
+        Elektronik hp = new Elektronik("samsung flip", hargaHP);
+        Pakaian hoodie = new Pakaian("Balenciaga", hargaHoodie);
+        
+        //Menghitung diskonnya
+        double diskonUmum = 0.3;
+        double diskonNovel = 0.15;
+        double diskonHp = 0.2;
+        double diskonHoodie = 0.25;
+        
+        umum.hitungDiskon(diskonUmum);
+        novel.hitungDiskon(diskonNovel);
+        hp.hitungDiskon(diskonHp);
+        hoodie.hitungDiskon(diskonHoodie);
+        
+        // Memasukkan Keranjang
+        Keranjang keranjangBelanja = new Keranjang();
+        keranjangBelanja.tambahKeranjang(umum);
+        keranjangBelanja.tambahKeranjang(novel);
+        keranjangBelanja.tambahKeranjang(hp);
+        keranjangBelanja.tambahKeranjang(hoodie);
+        
+        //Info produk    
+        System.out.println("\nproduk sepatu, harga awal: " + hargaUmum + ", diskon: " + (diskonUmum * 100) + "%");
+        System.out.println("produk novel: " + novel.nama + ", harga awal: " + hargaNovel + ", diskon: " + (diskonNovel * 100) + "%");
+        System.out.println("produk elektronik: " + hp.nama + ", harga awal: " + hargaHP + ", diskon: " + (diskonHp * 100) + "% + potongan Rp 50.000");
+        System.out.println("produk elektronik: " + hoodie.nama + ", harga awal: " + hargaHoodie + ", diskon: " + (diskonHoodie * 100) + "% + potongan Rp 100.000");
     
+    
+        System.out.println("\nIsi Keranjang dan total harga");
+        keranjangBelanja.isiKeranjang();
+        System.out.println("Total harga setelah diskon: " + keranjangBelanja.totalHarga());
+    }
 }
